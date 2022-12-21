c78489d36be360e8d9c629a3d1ace0600146c18b
job <hujunjob@qq.com>
2022-12-21 16:04:55 +0800

    Update：完成了币安合约交易实测

--- trade-sample.py
+++ trade-sample.py
@@ -2,12 +2,42 @@ import os
 from env import APIKEY,SECRET
 import ccxt
 
-exchange_id = 'binance'
-exchange_class = getattr(ccxt, exchange_id)
-exchange = exchange_class({
-    'apiKey': APIKEY,
-    'secret': SECRET,
-})
+binance = ccxt.binance(
+    {
+        "proxies": {
+            "http": "http://127.0.0.1:7890",
+            "https": "http://127.0.0.1:7890",
+        },
+        'apiKey': APIKEY,
+        'secret': SECRET,
+    }
+)
 
-exchange.fapiPrivateGetAccount()
-     
+def open_margin(_symbol,_direction,_amount):
+    # _symbol = "BTCUSDT"
+    # _direction = "BUY"
+    # _direction = "SELL"
+    # _amount=0.001
+
+    order_res = binance.fapiPrivatePostOrder({
+        "symbol": _symbol,
+        "side": _direction,
+        "type": "MARKET",
+        "positionSide":"LONG",
+        "quantity": _amount
+    })
+    orderid = order_res['orderId']
+    print(order_res)
+
+open_margin("BTCUSDT","BUY",0.001)
+
+# result = binance.fapiPrivateGetAccount()
+# result = binance.fapiPrivateV2GetAccount()
+# assets = result['assets']
+# positions = result['positions']
+
+# for position in positions:
+#     if(abs(float( position['positionAmt']))>0.00001):
+#         print(position)
+    
+
